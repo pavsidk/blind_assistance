e4701623717ae268b8b2fb7ee9d248f7cffee817
Pavan Siddharth Kosuru <pavan.sid.k.bay@gmail.com>
2025-11-27 22:12:51 -0800

    changes
    Refactor code to encapsulate main logic in a function and improve hazard detection.

--- model.py
+++ model.py
@@ -2,6 +2,13 @@ import cv2
 from ultralytics import YOLO
 import time
 import math
+import json
+import heapq
+
+
+#hazards is a list containing hazardous objects
+with open("hazards.json", "r") as f:
+    hazards = json.load(f)
 
 def compute_distance(x1, y1, x2, y2):
     return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
@@ -11,51 +18,92 @@ def compute_speed(dist, time1, time2):
         return 0
     return dist / (time2 - time1)
 
+#greater score -> closer the object is to the 
+def distance_score_from_bbox(y1, y2, frame_height):
+    bbox_h = max(0, y2 - y1)
+    if bbox_h <= 0:
+        return 0.0
+    return bbox_h / frame_height 
+
+def generate_danger(object_name, dist_score_int, speed):
+    for hazard in hazards:
+        if object_name == hazard:
+            return dist_score_int + speed
+    return 0
+
 
 model = YOLO("yolo11n.pt")
 cap = cv2.VideoCapture(0)
 
-obj_hist = {}
+def main():
+    best_scores = {}
+    
+    priority_queue = []
+    
+    obj_hist = {}
 
-while True:
+    while True:
+        dist_score_int = 0
+        if (len(priority_queue) > 1):
+            top_value = priority_queue[0]
+            
+            if (top_value[0] < -800):
+                obj = heapq.heappop(priority_queue)
+                print("watch out! there is an object, move or DIEEEEE")
 
-    ret, frame = cap.read()
-    if not ret:
-        break
+        ret, frame = cap.read()
+        if not ret:
+            break
 
-    results = model.track(frame, persist=True)[0]
+        frame_h, frame_w = frame.shape[:2] 
 
-    for b in results.boxes:
+        results = model.track(frame, persist=True)[0]
 
-        if b.id is None:
-            continue
-        obj_id = int(b.id[0])
+        for b in results.boxes:
+
+            if b.id is None:
+                continue
+            obj_id = int(b.id[0])
+            object_name = results.names[int(b.cls[0])]
+
+            seconds = time.perf_counter()
+            x_center, y_center, w, h = b.xywh[0]
+            x1, y1, x2, y2 = map(int, b.xyxy[0])
+
+            if obj_id not in obj_hist:
+                obj_hist[obj_id] = []
+
+            obj_hist[obj_id].append((seconds, float(x_center), float(y_center)))
+            speed = 0
+
+            if len(obj_hist[obj_id]) > 1:
+                dist_score = distance_score_from_bbox(y1, y2, frame_h)
+                dist_score_int = int(dist_score * 300)
+                
+                time1, x1center, y1center = obj_hist[obj_id][-2]
+                time2, x2center, y2center = obj_hist[obj_id][-1]
+
+                dist = compute_distance(x1center, y1center, x2center, y2center)
+                speed = compute_speed(dist, time1, time2)
+                
+                danger_score = generate_danger(object_name, dist_score_int, speed)
+                
+                if object_name in hazards:
+                    if object_name not in best_scores or danger_score > best_scores[object_name]:
+                        best_scores[object_name] = danger_score
+                        heapq.heappush(priority_queue, (-danger_score, object_name))
+                        print(priority_queue)
 
 
-        seconds = time.perf_counter()
-        x_center, y_center, w, h = b.xywh[0]
-        x1, y1, x2, y2 = map(int, b.xyxy[0])
+            
+            label = f"{results.names[int(b.cls[0])]} ID:{obj_id} Speed:{int(speed)}px/s Distance:{dist_score_int}"
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (0,255,0), 2)
+            cv2.putText(frame, label,  (x1, y1-5),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0,255,0), 2)
 
-        if obj_id not in obj_hist:
-            obj_hist[obj_id] = []
-
-        obj_hist[obj_id].append((seconds, float(x_center), float(y_center)))
-        speed = 0
-
-
-        if len(obj_hist[obj_id]) > 1:
-            time1, x1center, y1center = obj_hist[obj_id][-2]
-            time2, x2center, y2center = obj_hist[obj_id][-1]
-
-            dist = compute_distance(x1center, y1center, x2center, y2center)
-            speed = compute_speed(dist, time1, time2)
-
-        label = f"{results.names[int(b.cls[0])]} ID:{obj_id} Speed:{int(speed)}px/s"
-
-        cv2.rectangle(frame, (x1, y1), (x2, y2), (0,255,0), 2)
-        cv2.putText(frame, label, (x1, y1-5),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0,255,0), 2)
-
-    cv2.imshow("YOLO", frame)
-    if cv2.waitKey(1) == ord('q'):
-        break
+        cv2.imshow("YOLO", frame)
+        if cv2.waitKey(1) == ord('q'):
+            break
+        
+if __name__ == "__main__":
+    main()
